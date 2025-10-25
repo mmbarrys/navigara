80db636c588892edf541e34a59035d21a8143239
barry sahya <barrysahya@gmail.com>
2025-10-25 13:32:13 +0700

    Add console log for API_URL verification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,9 @@ import Nakhoda from './pages/Nakhoda';
 import LandingPage from './pages/LandingPage'; // <-- IMPORT BARU
 import { LayoutDashboard, Target, Activity, Network, User } from 'lucide-react';
 
-export const API_URL = 'http://localhost:5000';
+//export const API_URL = 'http://localhost:5000';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+console.log("API URL Used:", API_URL);
 
 const AppContext = createContext();
 export function useAppContext() { return useContext(AppContext); }
